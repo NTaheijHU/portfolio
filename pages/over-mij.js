6229130f7d877636fb2f15d609308b75e229adc1
ntaheij <noah@taheij.nl>
2021-09-22 09:37:36 +0200

    Added ImageCard

--- pages/over-mij.js
+++ pages/over-mij.js
@@ -1,3 +1,4 @@
+import ImageCard from "../components/cards/ImageCard";
 import Hero from "../components/hero/Hero";
 
 
@@ -9,7 +10,14 @@ export default function OverMij() {
   return (
     <div>
       <Hero hero={hero} />
-      <h1>Over Mij</h1>
+
+      {/* About Me */}
+      <div id="about-me">
+        <br />
+        <div className="container mb-12 mt-4 md:mt-8 mx-auto px-4 md:px-12">
+            <ImageCard />
+        </div>
+      </div>
     </div>
   );
 }
