360e82418f71fd240346cc342e5a6d28348126fa
ntaheij <noah@taheij.nl>
2021-09-22 15:08:20 +0200

    Fixed right align of stars

--- components/cards/reviews/ReviewCard.jsx
+++ components/cards/reviews/ReviewCard.jsx
@@ -2,16 +2,15 @@ function ReviewCard(props) {
   return ( 
     <div className="">
       <section className="overflow-hidden rounded-lg shadow-lg bg-gray-50 bg-opacity-90 mt-4 lg:mt-8">
-        <div className="lg:relative m-0 lg:grid lg:grid-cols-3">
-          <div className="p-6 lg:col-span-2">
+        <div className="lg:flex m-0">
+          <div className="p-6">
               <h3 className="uppercase font-medium text-2xl md:text-4xl md:mb-3">{props.name}</h3>
           </div>
-          {/* TODO: FIX IMAGE TO RIGHT */}
-          <div className="mb-4 ml-6 lg:my-4 lg:mx-4 lg:col-span-1 w-10/12 md:w-1/2">
-              <img alt="Stars" src={props.image} />
+          <div className="mb-4 ml-6 lg:ml-auto lg:my-5 lg:mr-4 w-3/4 md:w-1/6">
+            <img alt="Stars" src={props.image} />
           </div>
         </div>
-        <hr className="mx-2 border-b-4"/>
+        <hr className="ml-6 mx-2"/>
         <div className="p-6">
           <h2 className="leading-normal text-2xl font-semibold">{props.title}</h2>
           <p className="mt-2 text-xl">{props.text}</p>
