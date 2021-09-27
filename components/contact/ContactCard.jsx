38dc8aeca5cc6903ded4093ec854c9b6207ebaa1
ntaheij <noah@taheij.nl>
2021-09-27 12:16:33 +0200

    Fixed className Issue

--- components/contact/ContactCard.jsx
+++ components/contact/ContactCard.jsx
@@ -6,22 +6,22 @@ function ContactCard(props) {
           <div className="p-6 lg:col-span-2">
             <div className="lg:mt-4 lg:mx-4">
               <h2 className="uppercase font-medium text-3xl lg:text-4xl mb-3">{props.title}</h2>
-              <div class="flex justify-center mx-auto bg-gray-100 rounded-lg shadow-lg">
-                <form action="#" type="POST" class="w-full">
-                  <div class="p-3">
-                    <input class="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Naam" required />
+              <div className="flex justify-center mx-auto bg-gray-100 rounded-lg shadow-lg">
+                <form action="#" type="POST" className="w-full">
+                  <div className="p-3">
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Naam" required />
                   </div>
-                  <div class="p-3">
-                    <input class="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Adres" required />
+                  <div className="p-3">
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Adres" required />
                   </div>
-                  <div class="p-3">
-                    <input class="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Telefoon Nummer" required />
+                  <div className="p-3">
+                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Telefoon Nummer" required />
                   </div>
-                  <div class="p-3">
-                    <textarea class="resize-none rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border-light-blue-400 w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56" placeholder="Message" required></textarea>
+                  <div className="p-3">
+                    <textarea className="resize-none rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border-light-blue-400 w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56" placeholder="Message" required></textarea>
                   </div>
-                  <div class="p-3 pt-4">
-                  <button class="w-full bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-4 rounded text-2xl">
+                  <div className="p-3 pt-4">
+                  <button className="w-full bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-4 rounded text-2xl">
                   Send
                   </button>
                   </div>
