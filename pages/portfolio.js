0187c69d7b64431dd3fa2843ed6244a63f231edd
ntaheij <noah@taheij.nl>
2021-10-04 15:34:01 +0200

    Added dynmaics to projects page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,6 +4,9 @@ import ProjectCard from "../components/cards/portfolio/ProjectCard";
 
 import {useReducedMotion, motion} from "framer-motion";
 
+import aboutInfo from '../data/about.json';
+import skillsInfo from '../data/skills.json';
+import projectInfo from '../data/projects.json';
 
 export default function Portfolio() {
   const hero = {
@@ -14,6 +17,7 @@ export default function Portfolio() {
 
  	let animateIcon = shouldReduceMotion ? { scale: 1, color: '#1d4ed8', origin: 0 } : { scale: 1.2, color: '#1d4ed8', origin: 0 };
 
+  let iterProjects = 0;
 
   return (
     <div className="bg-gray-200 bg-opacity-60">
@@ -24,23 +28,20 @@ export default function Portfolio() {
         <br />
         <div className="container mb-12 mt-4 md:mt-8 mx-auto px-2 md:px-4">
             <ImageCard
-              title="Mijn Projecten"
-              text="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae odio voluptate alias. Non, ad velit? Placeat fugiat nam sapiente commodi, possimus quidem rerum maiores voluptate pariatur dignissimos, quibusdam error aspernatur?
-              Excepturi magnam ducimus sunt quam delectus nulla porro eius voluptate, maxime repellat. Esse accusantium quasi expedita nobis nostrum alias, ex officiis ullam officia, eos sequi saepe voluptate voluptatem nihil voluptas.
-              Earum, sunt harum. Ab quae temporibus pariatur iure suscipit ipsum maxime tenetur, dolorem minima fuga dolore nihil itaque iste quod corrupti rem facere sequi alias ad tempora adipisci culpa reiciendis.
-              Minima numquam nesciunt libero maxime voluptates voluptatibus laboriosam adipisci blanditiis culpa asperiores molestias aliquam ullam explicabo ratione reprehenderit officiis, autem quidem quae nobis ducimus magni ad. Repellendus obcaecati tenetur officia!"
-              image="https://picsum.photos/512/256/?random"
+              title={aboutInfo.projects.title}
+              text={aboutInfo.projects.details}
+              image={aboutInfo.projects.image}
             >
               <section>
                 <div className="flex leading-tight p-2 md:p-4">
-                  <a className="no-underline text-grey-darker mr-2" href="#">
+                  <a className="no-underline text-grey-darker mr-2" href={aboutInfo.projects.githubLink}>
                     <span className="hidden">Github</span>
                     <motion.i
                     whileHover= { animateIcon }
                     whileFocus= { animateIcon }
                     className="fab fa-github fa-2x"></motion.i>
                   </a>
-                  <a className="no-underline text-grey-darker mr-2" href="#">
+                  <a className="no-underline text-grey-darker mr-2" href={aboutInfo.projects.gitlabLink}>
                     <span className="hidden">Gitlab</span>
                     <motion.i
                     whileHover= { animateIcon }
@@ -57,49 +58,62 @@ export default function Portfolio() {
       <div id="projects">
         <div className="container mb-2 mt-4 md:mt-8 mx-auto px-2 md:px-4">
           <div className="grid grid-cols-2 md:grid-cols-8">
-            <a className="ml-2 mt-2 bg-gray-600 hover:bg-gray-600 text-gray-200 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out">
-              REACT
-            </a>
-            <a className="ml-2 mt-2 bg-gray-300 hover:bg-gray-600 text-gray-700 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out">
-              TAILWINDCSS
-            </a>
-            <a className="ml-2 mt-2 bg-gray-300 hover:bg-gray-600 text-gray-700 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out">
-              PYTHON
-            </a>
-            <a className="ml-2 mt-2 bg-gray-300 hover:bg-gray-600 text-gray-700 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out">
-              JAVA
-            </a>
+            {
+              skillsInfo.map((skill) => {
+                if(skillsInfo[0] === skill) {
+                  return (
+                    <a className="ml-2 mt-2 bg-gray-600 hover:bg-gray-600 text-gray-200 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out uppercase"
+                      key={skill.name}>
+                      {skill.name}
+                    </a>
+                  )
+                } else {
+                  return (
+                    <a className="ml-2 mt-2 bg-gray-300 hover:bg-gray-600 text-gray-700 hover:text-gray-200 font-semibold text-center text-md py-3 px-8 rounded-xl duration-200 ease-in-out uppercase"
+                      key={skill.name}>
+                      {skill.name}
+                    </a>
+                  )
+                }
+
+              })
+            }
           </div>
         </div>
         <div className="container mb-12 mx-auto px-2 md:px-4">
-          <ProjectCard
-              image="https://picsum.photos/600/400/?random"
-              title="Project 1"
-              link="/projects/1"
-              mirrored={true}
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur optio magnam perferendis blanditiis labore aliquam omnis culpa repellat, cum similique vitae? Distinctio, minima deleniti placeat veniam amet dolores corporis."
-              githubLink="https://github.com/ntaheij/project1"
-              websiteLink="https://ntaheij.github.io/project1"
-              
-          />
-          <ProjectCard 
-              image="https://picsum.photos/600/400/?random"
-              title="Project 1"
-              link="/projects/1"
-              mirrored={false}
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur optio magnam perferendis blanditiis labore aliquam omnis culpa repellat, cum similique vitae? Distinctio, minima deleniti placeat veniam amet dolores corporis."
-              githubLink="https://github.com/ntaheij/project1"
-              websiteLink="https://ntaheij.github.io/project1"
-          />
-          <ProjectCard 
-              image="https://picsum.photos/600/400/?random"
-              title="Project 1"
-              link="/projects/1"
-              mirrored={true}
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem tenetur optio magnam perferendis blanditiis labore aliquam omnis culpa repellat, cum similique vitae? Distinctio, minima deleniti placeat veniam amet dolores corporis."
-              githubLink="https://github.com/ntaheij/project1"
-              websiteLink="https://ntaheij.github.io/project1"
-          />
+          {
+            projectInfo.map((project) => {
+              iterProjects++;
+
+              if(iterProjects % 2 === 0) {
+                return (
+                  <ProjectCard
+                  key={project.name}
+                  image={project.image}
+                  title={project.name}
+                  link={project.link}
+                  mirrored={false}
+                  text={project.shortDescription}
+                  githubLink={project.githubLink}
+                  websiteLink={project.websiteLink}    
+                  />
+                )
+              } else {
+                return (
+                  <ProjectCard
+                  key={project.name}
+                  image={project.image}
+                  title={project.name}
+                  link={project.link}
+                  mirrored={true}
+                  text={project.shortDescription}
+                  githubLink={project.githubLink}
+                  websiteLink={project.websiteLink}
+                  />
+                )
+              }
+            })
+          }
         </div>
       </div>
     </div>
