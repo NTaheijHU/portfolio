de3e0600180470e28d5e238b2c610932903fef7d
ntaheij <noah@taheij.nl>
2021-10-07 09:31:23 +0200

    Made contact accesible

--- components/contact/ContactCard.jsx
+++ components/contact/ContactCard.jsx
@@ -8,16 +8,20 @@ function ContactCard(props) {
               <div className="flex justify-center mx-auto bg-gray-200 rounded-lg shadow-lg">
                 <form action="#" type="POST" className="w-full">
                   <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Naam" required />
+                    <label htmlFor="naam">Naam</label>
+                    <input id="naam" className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Naam" required />
                   </div>
                   <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Adres" required />
+                    <label htmlFor="email">Email</label>
+                    <input id="email" className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="email" placeholder="Email Adres" required />
                   </div>
                   <div className="p-3">
-                    <input className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Telefoon Nummer" required />
+                    	<label htmlFor="telefoonnummer">Telefoonnummer</label>
+                    <input id="telefoonnummer"className="block appearance-none placeholder-gray-500 placeholder-opacity-100 border border-light-blue-400 rounded-md w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300" type="text" placeholder="Telefoon Nummer" />
                   </div>
                   <div className="p-3">
-                    <textarea className="resize-none rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border-light-blue-400 w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56" placeholder="Message" required></textarea>
+                    <label htmlFor="bericht">Bericht</label>
+                    <textarea id="bericht" className="resize-none rounded-md block appearance-none placeholder-gray-500 placeholder-opacity-100 border-light-blue-400 w-full py-3 px-4 text-gray-700 leading-5 focus:outline-none focus:ring-2 focus:ring-light-blue-300 h-56" placeholder="Bericht" required></textarea>
                   </div>
                   <div className="p-3 pt-4">
                   <button className="w-full bg-gray-700 hover:bg-gray-900 text-white font-bold py-3 px-4 rounded text-2xl">
