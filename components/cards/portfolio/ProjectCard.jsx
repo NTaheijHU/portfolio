9c136a82fed8fa7c48238db729acc029a048cfb0
ntaheij <noah@taheij.nl>
2021-10-12 10:34:47 +0200

    Fixed linking issue on portfolio

--- components/cards/portfolio/ProjectCard.jsx
+++ components/cards/portfolio/ProjectCard.jsx
@@ -13,7 +13,11 @@ export default function ProjectCard(props) {
           <div className="lg:relative m-0 lg:grid lg:grid-cols-3">
             <div className="p-6 lg:col-span-2">
                 <h2 className="uppercase font-medium text-4xl mb-3">{props.title}</h2>
-                <p className="leading-normal text-xl">{props.text}</p>
+                <p className="leading-normal text-xl">{props.text}&nbsp;
+                  <a className="no-underline hover:underline text-blue-700" href={props.link}>
+                    Lees meer...
+                  </a>
+                </p>
             </div>
               <div className="lg:my-4 lg:mx-4 lg:col-span-1">
                 <img alt="Placeholder" src={props.image} />
@@ -111,7 +115,6 @@ export default function ProjectCard(props) {
                     Lees meer...
                   </a>
                 </p>
-                
             </div>
           </div>
         </section>
