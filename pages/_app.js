5d6df97a017ca469d93788d6e88ef4e578e87699
ntaheij <noah@taheij.nl>
2021-10-13 19:09:35 +0200

    Added dark mode logic

--- pages/_app.js
+++ pages/_app.js
@@ -8,8 +8,18 @@ import "../styles/arrow.css";
 import Head from "next/head";
 import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+      document.documentElement.classList.add('dark')
+    } else {
+      document.documentElement.classList.remove('dark')
+    }
+  });
+
   return (
     <>
       <Head>
@@ -20,7 +30,7 @@ function MyApp({ Component, pageProps }) {
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         />
       </Head>
-      <div className="bg-gray-200 bg-opacity-60" id="App">
+      <div className="bg-gray-200 bg-opacity-60 dark:bg-gray-800 dark:bg-opacity-100" id="App">
         <Header />
         <Component {...pageProps} />
         <Footer />
