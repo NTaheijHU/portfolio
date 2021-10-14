070bf18fefeb9703747ede7c9e2c46f56d6d4497
ntaheij <noah@taheij.nl>
2021-10-14 17:01:48 +0200

    Added random image to skills

--- components/cards/home/SkillCard.jsx
+++ components/cards/home/SkillCard.jsx
@@ -3,7 +3,7 @@ export default function SkillCard(props) {
     <div className="">
       <article className="overflow-hidden rounded-lg shadow-lg bg-gray-50 bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-100 dark:text-white">
         <a href="#" className="flex justify-center items-center mt-2 md:mt-4">
-                <img alt="Placeholder" className="block rounded-full" src="https://picsum.photos/48/48/?random" />
+                <img alt="Placeholder" className="block rounded-full" src={"https://picsum.photos/48/48/?random&" + props.title } />
         </a>
         <header className="p-2 md:p-4">
           <a className="no-underline text-black dark:text-gray-100" href={props.link}>
