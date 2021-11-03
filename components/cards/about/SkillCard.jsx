3dac026ab587e01da7dd834cc6f76d9911734025
ntaheij <noah@taheij.nl>
2021-11-03 12:43:29 +0100

    Made fixed height for skillcards

--- components/cards/about/SkillCard.jsx
+++ components/cards/about/SkillCard.jsx
@@ -1,7 +1,7 @@
 function SkillCard(props) {
   return ( 
     <div className="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/2">
-      <article className="overflow-hidden rounded-lg shadow-lg bg-gray-50 bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-100 dark:text-white mt-2 md:mt-4">
+      <article className="min-h-full overflow-hidden rounded-lg shadow-lg bg-gray-50 bg-opacity-90 dark:bg-gray-700 dark:bg-opacity-100 dark:text-white mt-2 md:mt-4">
         <header className="p-2 md:p-4">
           <a className="no-underline text-black dark:text-gray-100" href={props.link}>
             <h1 className="text-4xl font-bold text-center">
@@ -14,13 +14,13 @@ function SkillCard(props) {
             <p className="text-xl">
               {props.text}
             </p>
-            <p className="text-2xl pt-2">
-              <a className="no-underline hover:underline text-blue-700 dark:text-blue-500" href={props.link}>
-                Bekijk Projecten
-              </a>
-            </p>
           </div>
         </section>
+        <p className="text-2xl pt-2 text-center mb-2">
+          <a className="no-underline hover:underline text-blue-700 dark:text-blue-500" href={props.link}>
+            Bekijk Projecten
+          </a>
+        </p>
       </article>
     </div>
   );
